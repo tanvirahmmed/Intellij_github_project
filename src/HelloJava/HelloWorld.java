3ee4f45f10e5fc1a3b5b4942e8e3f0c7d301c62a
tanvirahmmed <rinko.ewu@gmail.com>
2016-08-05 16:08:07 -0400

    Initial commit

---
+++ src/HelloJava/HelloWorld.java
@@ -0,0 +1,10 @@
+package HelloJava;
+
+/**
+ * Created by rinko on 8/5/2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
